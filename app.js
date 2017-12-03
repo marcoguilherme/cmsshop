74fe5386f3d5b41c5d3ca632d203110f690357d8
marcoguilherme <mgbm13@gmail.com>
2017-12-03 19:51:02 -0200

    Novas dependencias instaladas

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var path = require('path');
 var config = require('./config/database');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var pages = require('./routes/pages.js');
+var adminPages = require('./routes/admin_pages.js');
 
 //MongoDM Connector
 var mongoose = require('mongoose');
@@ -15,16 +19,28 @@ db.once('open', function() {
 //Init app
 var app = express();
 
-app.get('/', (req, res)=>{
-    res.render('index');
-});
-
 //View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //Set public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Express Session
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+app.use('/', pages);
+app.use('/admin/pages', adminPages);
 
 //Start server
 var port = 3000;
